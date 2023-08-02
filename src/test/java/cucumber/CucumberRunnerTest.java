c2bb12dbb5a461437100fcdf5b7e2fc2a92e6c46
Nonzuzo Hlongwane <112972185+nonhlong021@users.noreply.github.com>
2023-08-02 16:44:53 +0200

    zoe

--- src/test/java/cucumber/CucumberRunnerTest.java
+++ src/test/java/cucumber/CucumberRunnerTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/feature"
         ,glue={"feature"},
-        tags="@rest"
+        tags="@"
 )
 
 public class CucumberRunnerTest {
